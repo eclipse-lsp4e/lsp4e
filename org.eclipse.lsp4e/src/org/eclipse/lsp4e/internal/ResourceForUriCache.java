eclipse-lsp4e/lsp4e

commit 3e789189e4dcc2bc731ff4466ae3d1cab44f9ea3

[#1207] cache IResource in EnablementTester (#1208)
Cache resource for URI because each call of
LSPEclipseUtils.findResourceFor(URI) takes ~300 microseconds. And it
gets called a lot of times.

fixes #1207

org.eclipse.lsp4e/src/org/eclipse/lsp4e/internal/ResourceForUriCache.java
@@ -1,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   See git history
+ *******************************************************************************/
+
+package org.eclipse.lsp4e.internal;
+
+import java.net.URI;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Adapters;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.lsp4e.LSPEclipseUtils;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+/**
+ * <p>NOTE: In case a resource has been moved or deleted the entry will not be removed automatically.
+ * It's up to the caller to check if the resource is accessible.
+ *
+ * <p>The cache is limited to 100 resource elements. It uses least-recently-used eviction if limit exceeds.
+ * The cache will try to evict entries that haven't been used recently.
+ * Therefore entries can be removed before the limit exceeds.
+ */
+public final class ResourceForUriCache {
+	private static final String FILE_SCHEME = "file"; //$NON-NLS-1$
+	private static final Cache<URI, IResource> cache = CacheBuilder.newBuilder().maximumSize(100).build();
+
+	private ResourceForUriCache() {
+		// this class shouldn't be instantiated
+	}
+
+	/**
+	 * <p>Returns the cached IResource for the given URI. Tries to determine the IResource
+	 * if it's not already in the cache. Returns NULL if the IResource could not be determined,
+	 * e.g. the URI points to a file outside the workspace.
+	 *
+	 * <p>NOTE: In case a resource has been moved or deleted the entry will not be removed automatically.
+	 * It's up to the caller to check if the resource is accessible.
+	 * @param uri
+	 * @return IResource or NULL
+	 */
+	@Nullable
+	public static IResource get(@Nullable URI uri) {
+		// Note: The load method in CacheLoader/LoadingCache cannot be applied here because
+		// the load method has to return a non-null value.
+		// But it cannot be guaranteed that there can be a IResource fetched for the given URI.
+		URI localURI = uri;
+		IResource resource = null;
+		if (localURI != null) {
+			resource = cache.getIfPresent(localURI);
+			if (resource != null) {
+				return resource;
+			}
+			resource = findResourceFor(localURI);
+			if (resource != null) {
+				cache.put(localURI, resource);
+			}
+		}
+		return resource;
+	}
+
+	@Nullable
+	private static IResource findResourceFor(URI uri) {
+		if (FILE_SCHEME.equals(uri.getScheme())) {
+			IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
+
+			IFile[] files = wsRoot.findFilesForLocationURI(uri);
+			if (files.length > 0) {
+				IFile file = LSPEclipseUtils.findMostNested(files);
+				if(file!=null) {
+					return file;
+				}
+			}
+
+			return ArrayUtil.findFirst(wsRoot.findContainersForLocationURI(uri));
+		} else {
+			return Adapters.adapt(uri, IResource.class, true);
+		}
+	}
+
+}
+
+

org.eclipse.lsp4e/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: Language Server Protocol client for Eclipse IDE (Incubation)
 Bundle-SymbolicName: org.eclipse.lsp4e;singleton:=true
-Bundle-Version: 0.18.17.qualifier
+Bundle-Version: 0.18.18.qualifier
 Bundle-RequiredExecutionEnvironment: JavaSE-17
 Require-Bundle: org.eclipse.core.runtime;bundle-version="3.12.0",
  org.eclipse.equinox.common;bundle-version="3.8.0",

org.eclipse.lsp4e/pom.xml
@@ -10,7 +10,7 @@
 	</parent>
 	<artifactId>org.eclipse.lsp4e</artifactId>
 	<packaging>eclipse-plugin</packaging>
-	<version>0.18.17-SNAPSHOT</version>
+	<version>0.18.18-SNAPSHOT</version>
 
 	<build>
 		<plugins>

org.eclipse.lsp4e/src/org/eclipse/lsp4e/LSPEclipseUtils.java
@@ -94,6 +94,7 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.lsp4e.internal.ArrayUtil;
 import org.eclipse.lsp4e.internal.DocumentInputStream;
+import org.eclipse.lsp4e.internal.ResourceForUriCache;
 import org.eclipse.lsp4e.refactoring.CreateFileChange;
 import org.eclipse.lsp4e.refactoring.DeleteExternalFile;
 import org.eclipse.lsp4e.refactoring.LSPTextChange;
@@ -458,24 +459,7 @@ public final class LSPEclipseUtils {
 
 	@Nullable
 	public static IResource findResourceFor(@Nullable URI uri) {
-		if (uri == null) {
-			return null;
-		}
-		if (FILE_SCHEME.equals(uri.getScheme())) {
-			IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
-
-			IFile[] files = wsRoot.findFilesForLocationURI(uri);
-			if (files.length > 0) {
-				IFile file = findMostNested(files);
-				if(file!=null) {
-					return file;
-				}
-			}
-
-			return ArrayUtil.findFirst(wsRoot.findContainersForLocationURI(uri));
-		} else {
-			return Adapters.adapt(uri, IResource.class, true);
-		}
+		return ResourceForUriCache.get(uri);
 	}
 
 	public static @Nullable IFile findMostNested(IFile[] files) {

commit dc0156277e4eecff4918a238291ab45dc5f5b927

fix: automatically invalidate moved/deleted entries from the ResourceForUriCache
If a resource is moved or deleted, it's possible that the resource
stored for its URI in the ResourceForUriCache stops being `accessible`.

This commit changes `ResourceForUriCache.get(URI)` so that, if a
resource is present in the cache but is no longer accessible, the
corresponding cache entry is invalidated. In this case, `.get(URI)` then
tries to determine the up-to-date IResource again.

Fixes a regression introduced in #1208. Specifically, this commit
addresses the concern raised in
https://github.com/eclipse-lsp4e/lsp4e/pull/1208#discussion_r1942357586

org.eclipse.lsp4e/src/org/eclipse/lsp4e/internal/ResourceForUriCache.java
@@ -46,8 +46,8 @@ public final class ResourceForUriCache {
 	 * if it's not already in the cache. Returns NULL if the IResource could not be determined,
 	 * e.g. the URI points to a file outside the workspace.
 	 *
-	 * <p>NOTE: In case a resource has been moved or deleted the entry will not be removed automatically.
-	 * It's up to the caller to check if the resource is accessible.
+	 * <p>In case a resource has been moved or deleted, the cache entry will be invalidated,
+	 * and this method will re-attempt to find the IResource.
 	 * @param uri
 	 * @return IResource or NULL
 	 */
@@ -61,7 +61,10 @@ public final class ResourceForUriCache {
 		if (localURI != null) {
 			resource = cache.getIfPresent(localURI);
 			if (resource != null) {
-				return resource;
+				if (resource.isAccessible()) {
+					return resource;
+				}
+				cache.invalidate(localURI);
 			}
 			resource = findResourceFor(localURI);
 			if (resource != null) {